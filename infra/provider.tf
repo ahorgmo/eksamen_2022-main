d7b2acdd4db2d07eba13a697d2bcc02d54f9861c
EC2 Default User <ec2-user@ip-172-31-35-219.eu-west-1.compute.internal>
2022-12-07 22:15:46 +0000

    update

--- infra/provider.tf
+++ infra/provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "2.49.0"
+      version = "4.40.0"
     }
   }
   backend "s3" {
     bucket = "analytics-1038"
     key    = "1038/terraform-eksamen.state"
-    region = "eu-north-1"
+    region = "eu-west-1"
   }
 }

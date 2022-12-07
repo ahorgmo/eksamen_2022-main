fa1275c46c85b817cf7ebc55571b36044c0bc8c4
EC2 Default User <ec2-user@ip-172-31-35-219.eu-west-1.compute.internal>
2022-12-07 20:38:46 +0000

    added dashboard watch

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -22,6 +22,63 @@ resource "aws_cloudwatch_dashboard" "main" {
         "region": "eu-west-1",
         "title": "Total number of checkouts"
       }
+    },
+    {
+      "type": "metric",
+       "x": 0,
+       "y": 0,
+       "width": 12,
+       "height": 6,
+       "properties": {
+         "metrics": [
+           [
+             "${var.candidate_id}",
+             "cartsvalue_count.value"
+           ]
+         ],
+         "period": 300,
+         "stat": "Maximum",
+         "region": "eu-west-1",
+         "title": "Total number of value in carts"
+      }
+    },
+    {
+      "type": "metric",
+       "x": 0,
+       "y": 0,
+       "width": 12,
+       "height": 6,
+       "properties": {
+         "metrics": [
+           [
+             "${var.candidate_id}",
+             "carts_count.value"
+           ]
+         ],
+         "period": 300,
+         "stat": "Maximum",
+         "region": "eu-west-1",
+         "title": "Total number of carts"
+      }
+    },
+    {
+      "type": "metric",
+       "x": 0,
+       "y": 0,
+       "width": 12,
+       "height": 6,
+       "properties": {
+         "metrics": [
+           [
+             "${var.candidate_id}",
+             "no.shoppifly.ShoppingCartController none checkout method.timed.sum"
+           ]
+         ],
+         "period": 300,
+         "stat": "Maximum",
+         "region": "eu-west-1",
+         "title": "Total response time for checkout method"
+      }
     }
   ]
 }
